a6cb194ee8dc9424e2a6a309f2cf79895286fafc
Arif Hasnat <arif.hasnat@dreamonline.co.jp>
2021-10-07 10:19:11 +0600

    pdf

---
+++ Shared/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by DREAMONLINE on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("DreamOnline!")
+                .font(.title)
+                .padding()
+            
+            Text("software company!")
+                .padding()
+                
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+
+    }
+}
---
+++ Shared/zczxApp.swift
@@ -0,0 +1,17 @@
+//
+//  zczxApp.swift
+//  Shared
+//
+//  Created by DREAMONLINE on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct zczxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
